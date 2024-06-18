209c4c5339bec0b673eda0ad9149100654896407
Aithan Eulysse Gimenez <gimenezaithan@gmail.com>
2024-06-18 19:28:02 +0800

    removed eloquent commits

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\RelationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('products', ProductController::class);
-Route::get('/author/books/{id}', [RelationController::class, 'getBooksByAuthor']);
